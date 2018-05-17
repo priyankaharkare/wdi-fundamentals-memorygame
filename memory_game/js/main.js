e2065bd42f67c82db8e338f32673c768b251882c
Sankalp Sethi <sethi@Sankalps-MacBook-Air.local>
2018-05-16 20:45:07 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
-console.log("Up and running !");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king" , "king"];
+var cardsInPlay = [];
+var cardOne;
+
+var cardOne = cards[0];
+
+cardsInPlay.push('cards[0]');
+console.log("User flipped queen");
+
+var cardTwo;
+var cardTwo = cards[2];
+cardsInPlay.push('cards[2]');
+console.log("user flipped king");
+
+if (length.cardsInPlay === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert ( " You found a match ");
+	}
+	else{
+		alert("Sorry, try again");
+	}
+
